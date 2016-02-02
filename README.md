3c0bf5402f7f89b53e8c41e96997126bd1af5835
Mudream <mudream4869@gmail.com>
2016-02-02 13:25:40 +0800

    add story catcher

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # baidutieba-tool
 一些百度貼吧的腳本
+
+# 使用方法
+
+## catchstory
+
+```
+python catcgstory.py [帖子url]
+```
---
+++ catchstory.py
@@ -0,0 +1,121 @@
+import BeautifulSoup as bs
+import requests
+import sys
+
+def ch2num(ch):
+    arr = "0123456789"
+    for x in range(10):
+        if ch == arr[x]:
+            return x
+    return 0
+
+def getLastNum(input_str):
+    ret, a10 = 0, 1
+    for ch in input_str[::-1]:
+        if ch != '=':
+            ret = ret + ch2num(ch)*a10
+            a10 *= 10
+        else:
+            break
+    return ret
+
+def removetag(input_str, tagname):
+    input_str = input_str.replace("</" + tagname + ">", "")
+    
+    while True:
+        get_str = "<" + tagname
+        l = len(get_str)
+        s_ind = -1
+        for i in range(len(input_str)):
+            if i >= l and input_str[i-l:i] == "<" + tagname :
+                s_ind = i
+                break
+        if s_ind == -1 : break
+
+        for i in range(len(input_str)):
+            if i < s_ind : continue
+            get_str = get_str + input_str[i]
+            if input_str[i] == ">" : break
+
+        input_str = input_str.replace(get_str, "")
+
+    return input_str
+
+def dealcontent(input_str):
+    input_str = input_str.replace("<br />", "\n")
+    input_str = removetag(input_str, "a")
+    input_str = removetag(input_str, "div")
+    input_str = removetag(input_str, "img")
+    input_str = removetag(input_str, "span")
+
+    return input_str
+
+story_url = sys.argv[1]
+
+print("Story url = " + story_url)
+
+res = requests.get(story_url)
+
+if res.status_code != 200 :
+    print("Error occur : when try to get request by url.")
+    exit()
+
+soup = bs.BeautifulSoup(res.text)
+posts_div = soup.findAll("div", {"id": "j_p_postlist"})
+
+if not len(posts_div):
+    print("Error occur : no post exists.")
+    exit()
+
+## First : Get author
+
+author_name = posts_div[0].findAll("a", {"class" : lambda x : x and "p_author_name" in x.split()})[0].text
+
+print("Author = " + author_name)
+
+## Second : Get Last page number
+
+pages_a = soup.findAll("li", 
+    {"class" : "l_pager pager_theme_4 pb_list_pager"}
+)[0].findAll("a")
+
+last_page_num = getLastNum(pages_a[len(pages_a)-1].get("href"))
+
+print("Page Count = " + str(last_page_num))
+
+## Third : Seive post
+
+for i in range(last_page_num):
+    page = i + 1
+    curent_url = story_url + "?pn=" + str(page)
+
+    res = requests.get(curent_url)
+
+    if res.status_code != 200 :
+        print("Error occur : when try to get request by url at page %d."%(page,))
+        continue
+
+    soup = bs.BeautifulSoup(res.text)
+
+    posts_div_list = soup.findAll("div", 
+        {"class" : lambda x : x and "l_post" in x.split()}
+    )
+
+    print("Count = " + str(len(posts_div_list)))
+
+    for post in posts_div_list:
+        post_author_name = post.findAll("a", 
+            {"class" : lambda x : x and "p_author_name" in x.split()}
+        )[0].text
+
+        if post_author_name != author_name : continue
+
+        id_name = "post_content"
+
+        content = post.findAll("div", 
+            {"id" : lambda x : x and len(x) > len(id_name) and x[0:len(id_name)] == id_name }
+        )[0]
+
+        print(dealcontent(str(content)))
+
+        print("----------------------------------------------------------")
